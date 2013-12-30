87bbbf5aa6c1d990808d48a9bdde9a218404e6aa
prakhar-goel <prakhar.goel@inmobis-MacBook-Pro-2.local>
2013-12-30 21:08:01 +0530

    changed mysql query

--- search-and-edit-users.php
+++ search-and-edit-users.php
@@ -3,50 +3,12 @@
 <?php include('includes/subscribers/main.php');?>
 <?php include('includes/helpers/short.php');?>
 <?php
-	//vars
-	if(isset($_GET['s'])) $s = $_GET['s'];
-	else $s = '';
-
-/*
-	if(get_app_info('is_sub_user')) 
-	{
-		if(get_app_info('app')!=get_app_info('restricted_to_app'))
-		{
-			echo '<script type="text/javascript">window.location="'.get_app_info('path').'/list?i='.get_app_info('restricted_to_app').'"</script>';
-			exit;
-		}
-		$q = 'SELECT app FROM lists WHERE id = '.mysqli_real_escape_string($mysqli, $_GET['l']);
-		$r = mysqli_query($mysqli, $q);
-		if ($r)
-		{
-		    while($row = mysqli_fetch_array($r))
-		    {
-				$a = $row['app'];
-		    }  
-		    if($a!=get_app_info('restricted_to_app'))
-		    {
-			    echo '<script type="text/javascript">window.location="'.get_app_info('path').'/list?i='.get_app_info('restricted_to_app').'"</script>';
-				exit;
-		    }
-		}
-	}
+	$manual_log = "::::MANUAL:::: ";
 	
 	//vars
 	if(isset($_GET['s'])) $s = $_GET['s'];
 	else $s = '';
-	if(isset($_GET['c'])) $c = $_GET['c'];
-	else $c = '';
-	if(isset($_GET['p'])) $p = $_GET['p'];
-	else $p = '';
-	if(isset($_GET['a'])) $a = $_GET['a'];
-	else $a = '';
-	if(isset($_GET['u'])) $u = $_GET['u'];
-	else $u = '';
-	if(isset($_GET['b'])) $b = $_GET['b'];
-	else $b = '';
-	if(isset($_GET['cp'])) $cp = $_GET['cp'];
-	else $cp = '';
-*/
+
 ?>
 <div class="row-fluid">
     <div class="span2">
@@ -54,52 +16,7 @@
     </div> 
     <div class="span10">
     	<h2><?php echo _('Search and Edit Users');?></h2> <br/>
-<!-- 
-    	<button class="btn" onclick="window.location='<?php echo get_app_info('path');?>/update-list?i=<?php echo get_app_info('app');?>&l=<?php echo $_GET['l'];?>'"><i class="icon-plus-sign"></i> <?php echo _('Add subscribers');?></button> 
-    	<button class="btn" onclick="window.location='<?php echo get_app_info('path');?>/delete-from-list?i=<?php echo get_app_info('app');?>&l=<?php echo $_GET['l'];?>'"><i class="icon-minus-sign"></i> <?php echo _('Delete subscribers');?></button> 
-    	<button class="btn" onclick="window.location='<?php echo get_app_info('path');?>/unsubscribe-from-list?i=<?php echo get_app_info('app');?>&l=<?php echo $_GET['l'];?>'"><i class="icon-ban-circle"></i> <?php echo _('Mass unsubscribe');?></button>
--->
-    	<?php 
-    		//export according to which section user is on
-/*
-    		if($a=='' && $c=='' && $u=='' && $b=='' && $cp=='')
-    		{
-	    		$filter = '';
-	    		$filter_val = '';
-	    		$export_title = _('all subscribers');
-    		}
-    		else if($a!='')
-    		{
-	    		$filter = 'a';
-	    		$filter_val = $a;
-	    		$export_title = _('active subscribers');
-    		}
-    		else if($c!='')
-    		{
-	    		$filter = 'c';
-	    		$filter_val = $c;
-	    		$export_title = _('unconfirmed subscribers');
-    		}  
-    		else if($u!='')
-    		{
-	    		$filter = 'u';
-	    		$filter_val = $u;
-	    		$export_title = _('unsubscribers');
-    		} 
-    		else if($b!='')
-    		{
-	    		$filter = 'b';
-	    		$filter_val = $b;
-	    		$export_title = _('bounced subscribers');
-    		}
-    		else if($cp!='')
-    		{
-	    		$filter = 'cp';
-	    		$filter_val = $cp;
-	    		$export_title = _('subscribers who marked your email as spam');
-    		}
-*/
-    	?>
+
 		<form class="form-search" action="<?php echo get_app_info('path');?>/search-and-edit-users" method="GET" style="float:left;">
     		<input type="hidden" name="i" value="<?php echo get_app_info('app');?>">
 			<input type="text" class="input-medium search-query" name="s" value="<?php echo $s;?>">
@@ -114,13 +31,7 @@
 			<?php 
 				if($s!="")
 				{
-					$additional_clause = "";
-					if (get_app_data('app_name')!="")
-					{
-						$app_id = get_app_info('app');
-						$additional_clause = "AND a.id=".$app_id;
-					}
-					
+									
 					$q = 'SELECT s.id sid, s.userID,s.name ,s.email,s.custom_fields,s.list,s.unsubscribed,s.bounced,s.bounce_soft,s.complaint, s.timestamp,
 					s.confirmed, l.id lid, l.name lname, a.id aid, a.app_name aname
 					FROM subscribers s
@@ -131,9 +42,15 @@
 					OR s.name like "%'.mysqli_real_escape_string($mysqli, $s).'%")
 					ORDER by a.id,l.id,s.id asc LIMIT 200';
 
-					// echo $q;
+					$msc=microtime(true);
 					$r = mysqli_query($mysqli, $q);
-					$all_rows = mysqli_fetch_all($r, MYSQLI_ASSOC);
+					$msc=microtime(true)-$msc;
+					error_log($manual_log."MYSQL Time Taken: $msc secs");
+					$all_rows = array();					
+					while ($row = mysqli_fetch_assoc($r)) {
+					  $all_rows[] = $row;
+					}
+
 					$array_thisbrand = array();
 					$array_otherbrands = array();
 					foreach ($all_rows as $row) 
@@ -143,9 +60,6 @@
 						else
 							array_push($array_otherbrands, $row);
 					}
-					// print_r($array_thisbrand);
-					// echo "----------------";
-					// print_r($array_otherbrands);
 
 					function printrowheader()
 					{
@@ -216,14 +130,12 @@
 					if(count($array_otherbrands)>0)
 					{
 						?><tr><td colspan="8"><br><h2><?echo count($array_otherbrands)?> Results also found in Other Brands<h2></td></tr><?
-						//printrowheader();
 						foreach ($array_otherbrands as $row) {
 							printrow($row);
 						}
 					}
 					if(count($array_thisbrand)==0 && count($array_otherbrands)==0)
 					{
-						//printrowheader();
 						?>
 						<tr>
 		  				<td colspan="8">No subscribers found.</td>
